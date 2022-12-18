e2c80e9e807882339a2676c422f185f75056317e
Mickael <mickaelfelizardo2008@gmail.com>
2022-12-18 11:13:54 -0300

    carousel component

---
+++ src/ui/components/data-display/Carousel.tsx
@@ -0,0 +1,89 @@
+'use client';
+import { motion, AnimatePresence } from 'framer-motion';
+import { useCallback, useEffect, useRef, useState } from 'react';
+import Styles from './Carousel.module.scss';
+
+interface CarouselProps {
+    children: React.ReactElement[];
+    changeInterval?: number;
+}
+
+export const Carousel: React.FC<CarouselProps> = ({
+    children,
+    changeInterval = 0,
+}) => {
+    const containerRef = useRef<HTMLDivElement>(null);
+    const [currentItemIndex, setCurrentItemIndex] = useState(0);
+
+    const nextPage = useCallback(() => {
+        setCurrentItemIndex(prev => (prev + 1) % children.length);
+    }, [children.length]);
+
+    const prevPage = useCallback(() => {
+        setCurrentItemIndex(prev => (prev - 1) % children.length);
+    }, [children.length]);
+
+    useEffect(() => {
+        const container = containerRef.current;
+        let intervalId: number;
+
+        if (changeInterval > 0 && container) {
+            const clearInterval = () => {
+                window.clearInterval(intervalId);
+            };
+
+            const setInterval = () => {
+                clearInterval();
+
+                intervalId = window.setInterval(() => {
+                    nextPage();
+                }, changeInterval * 1000);
+            };
+
+            setInterval();
+
+            container.addEventListener('mouseover', clearInterval);
+            container.addEventListener('mouseleave', setInterval);
+
+            return () => {
+                window.clearInterval(intervalId);
+                container.removeEventListener('mouseover', clearInterval);
+                container.removeEventListener('mouseleave', setInterval);
+            };
+        }
+    }, [changeInterval, nextPage]);
+
+    return (
+        <div className={Styles.container} ref={containerRef}>
+            <button
+                className={Styles.button}
+                type="button"
+                aria-label="Anterior"
+                role="button"
+                onClick={() => prevPage()}
+            >
+                <i className="ri-arrow-left-s-line"></i>
+            </button>
+            <button
+                className={Styles.button}
+                type="button"
+                aria-label="Próximo"
+                role="button"
+                onClick={() => nextPage()}
+            >
+                <i className="ri-arrow-right-s-line"></i>
+            </button>
+            <AnimatePresence>
+                <motion.div
+                    initial={{ position: 'absolute', opacity: 0 }}
+                    animate={{ position: 'relative', opacity: 1 }}
+                    exit={{ position: 'absolute', opacity: 0 }}
+                    transition={{ duration: 0.3 }}
+                    key={currentItemIndex}
+                >
+                    {children.at(currentItemIndex)}
+                </motion.div>
+            </AnimatePresence>
+        </div>
+    );
+};
